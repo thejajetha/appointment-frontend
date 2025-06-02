8c326e0cb798a7c039e259cdf21386aad8a5e009
thejajetha <thejajethawork@gmail.com>
2025-06-02 09:10:48 +0530

    fourth

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export async function signUpApi(data){
-    return await axios.post("http://127.0.0.1:8000/api/register/",data)
+    return await axios.post("https://thejajetha.pythonanywhere.com/api/register/",data)
 }
 
 export async function getTokenApi(data){
-    return await axios.post("http://127.0.0.1:8000/api/token/",data)
+    return await axios.post("https://thejajetha.pythonanywhere.com/api/token/",data)
 
 }
 
@@ -14,15 +14,15 @@ export async function createAppointmentApi(data){
     console.log(token);
     
     let headers={Authorization:token}
-    return await axios.post("http://127.0.0.1:8000/api/appointments/",data,{headers})
+    return await axios.post("https://thejajetha.pythonanywhere.com/api/appointments/",data,{headers})
 }
 
 export async function getAvailableSlotApi(date){
-    return await axios.get(`http://127.0.0.1:8000/api/appointments/slots/?date=${date}`)
+    return await axios.get(`https://thejajetha.pythonanywhere.com/api/appointments/slots/?date=${date}`)
 }
 
 export async function appointmentListApi(){
     let token=localStorage.getItem("token")
     let headers={Authorization:token}
-    return await axios.get("http://127.0.0.1:8000/api/appointments/",{headers})
+    return await axios.get("https://thejajetha.pythonanywhere.com/api/appointments/",{headers})
 }
